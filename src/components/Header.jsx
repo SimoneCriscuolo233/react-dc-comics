f09e6efff9f20cd3c2089a5a8c44fbf94153fe09
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-08-25 15:21:42 +0200

    feat: add Header component with styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="container-fluid">
+        <div className="d-flex">
+          <div id="logo">
+            <img src="../public/img/dc-logo.png" alt="Logo" />
+          </div>
+          <div className="list">
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#" className="active">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Collectibles</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">Fans</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Shop</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </header>
+
+
+  )
+}
+
+export default Header
