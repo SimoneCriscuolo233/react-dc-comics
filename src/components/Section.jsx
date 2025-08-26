1de3a461ebb119530d72fa6517f4c88a2e7ff06c
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-08-26 09:53:19 +0200

    mnfix: update Header and Section components with improved layout and styling

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 const Section = () => {
   return (
     <section className='bg-blue'>
-      <ul className="d-flex justify-between">
-        <li className='d-flex t-white align-center'><img src="/img/buy-comics-digital-comics.png" alt="" className='shop-img' /> <span>DIGITAL COMICS</span></li>
+      <div className="container">
+        <ul className="d-flex justify-between">
+          <li className='d-flex t-white align-center'><img src="/img/buy-comics-digital-comics.png" alt="" className='shop-img' /> <span>DIGITAL COMICS</span></li>
 
-        <li className='d-flex t-white align-center'><img src="/img/buy-comics-merchandise.png" alt="" className='shop-img' /> <span>DC MERCHANDISE</span></li>
+          <li className='d-flex t-white align-center'><img src="/img/buy-comics-merchandise.png" alt="" className='shop-img' /> <span>DC MERCHANDISE</span></li>
 
-        <li className='d-flex t-white align-center'><img src="/img/buy-comics-subscriptions.png" alt="" className='shop-img' /> <span>SUBSCRIPTIONS</span></li>
+          <li className='d-flex t-white align-center'><img src="/img/buy-comics-subscriptions.png" alt="" className='shop-img' /> <span>SUBSCRIPTIONS</span></li>
 
-        <li className='d-flex t-white align-center'><img src="/img/buy-comics-shop-locator.png" alt="" className='shop-img' /> <span>COMIC SHOP LOCATOR</span></li>
+          <li className='d-flex t-white align-center'><img src="/img/buy-comics-shop-locator.png" alt="" className='shop-img' /> <span>COMIC SHOP LOCATOR</span></li>
 
-        <li className='d-flex t-white align-center'><img src="/img/buy-dc-power-visa.svg" alt="" className='shop-img' /><p>DC POWER VISA</p></li>
+          <li className='d-flex t-white align-center'><img src="/img/buy-dc-power-visa.svg" alt="" className='shop-img' id='svg' /><span>DC POWER VISA</span></li>
 
-      </ul>
+        </ul>
+      </div>
 
     </section>
   )
