384baff9e70027ff3eaf2af13e862317618683df
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-08-27 13:33:05 +0200

    feat: ! edit App component to manage menu, comics and footer data;
    mnfix: Adjust image paths in CSS for correct asset loading;
    
    update Header and Main components to accept props. Modify Footer component to utilize passed footerLists.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,88 +1,50 @@
-const Footer = () => {
-  const footerLists = [
-    {
-      id: 1,
-      title: "DC Comics",
-      list: [
-        { id: 1, href: "#", label: "Characters" },
-        { id: 2, href: "#", label: "Comics" },
-        { id: 3, href: "#", label: "Movies" },
-        { id: 4, href: "#", label: "TV" },
-        { id: 5, href: "#", label: "Games" },
-        { id: 6, href: "#", label: "Videos" },
-        { id: 7, href: "#", label: "News" }
-      ]
-    },
-    {
-      id: 2,
-      title: "Shop",
-      list: [
-        { id: 1, href: "#", label: "Shop DC" },
-        { id: 2, href: "#", label: "Shop DC Collectibles" }
-      ]
-    },
-    {
-      id: 3,
-      title: "DC",
-      list: [
-        { id: 1, href: "#", label: "Terms Of Use" },
-        { id: 2, href: "#", label: "Privacy Policy (New)" },
-        { id: 3, href: "#", label: "Ad Choices" },
-        { id: 4, href: "#", label: "Advertising" },
-        { id: 5, href: "#", label: "Jobs" },
-        { id: 6, href: "#", label: "Subscriptions" },
-        { id: 7, href: "#", label: "Talent Workshops" },
-        { id: 8, href: "#", label: "CPSC Certificates" },
-        { id: 9, href: "#", label: "Ratings" },
-        { id: 10, href: "#", label: "Shop Help" },
-        { id: 11, href: "#", label: "Contact Us" }
-      ]
-    },
-    {
-      id: 4,
-      title: "Sites",
-      list: [
-        { id: 1, href: "#", label: "DC" },
-        { id: 2, href: "#", label: "MAD Magazine" },
-        { id: 3, href: "#", label: "DC Kids" },
-        { id: 4, href: "#", label: "DC Universe" },
-        { id: 5, href: "#", label: "DC Power Visa" }
-      ]
-    }
-  ];
-
+const Footer = ({ footerLists }) => {
   return (
     <footer className="footer">
       <div className="container py-50">
         <div className="row">
           <div className="col-50 d-flex">
             <div className="col-33">
-              {footerLists.slice(0, 2).map((section) => (
-                <div key={section.id}>
-                  <h4 className="t-white">{section.title}</h4>
-                  <ul>
-                    {section.list.map((item) => (
-                      <li key={item.id}>
-                        <a href={item.href}>{item.label}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
+
+              {footerLists.slice(0, 2).map((section) => {
+                const { id, title, list } = section
+                return (
+                  <div key={id}>
+                    <h4 className="t-white">{title}</h4>
+                    <ul>
+                      {list.map((item) => {
+                        const { id, href, label } = item
+                        return (
+                          <li key={id}>
+                            <a href={href}>{label}</a>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                )
+              })}
             </div>
+
             <div className="col-33 d-flex">
-              {footerLists.slice(2).map((section) => (
-                <div key={section.id} className="col-50 ms-20">
-                  <h4 className="t-white">{section.title}</h4>
-                  <ul>
-                    {section.list.map((item) => (
-                      <li key={item.id}>
-                        <a href={item.href}>{item.label}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
+              {footerLists.slice(2).map((section) => {
+                const { id, title, list } = section
+                return (
+                  <div key={id} className="col-50 ms-20">
+                    <h4 className="t-white">{title}</h4>
+                    <ul>
+                      {list.map((item) => {
+                        const { id, href, label } = item
+                        return (
+                          <li key={id}>
+                            <a href={href}>{label}</a>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                )
+              })}
             </div>
 
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,73 +1,10 @@
-const Header = () => {
-  const menu = [
-    {
-      id: 1,
-      label: "Characters",
-      href: "#",
-      active: false
-    },
-    {
-      id: 2,
-      label: "Comics",
-      href: "#",
-      active: true
-    },
-    {
-      id: 3,
-      label: "Movies",
-      href: "#",
-      active: false
-    },
-    {
-      id: 4,
-      label: "Tv",
-      href: "#",
-      active: false
-    },
-    {
-      id: 5,
-      label: "Games",
-      href: "#",
-      active: false
-    },
-    {
-      id: 6,
-      label: "Collectibles",
-      href: "#",
-      active: false
-    },
-    {
-      id: 7,
-      label: "Videos",
-      href: "#",
-      active: false
-    },
-    {
-      id: 8,
-      label: "Fans",
-      href: "#",
-      active: false
-    },
-    {
-      id: 9,
-      label: "News",
-      href: "#",
-      active: false
-    },
-    {
-      id: 10,
-      label: "Shop",
-      href: "#",
-      active: false
-    },
-  ]
+const Header = ({ menu }) => {
   return (
     <header>
-
       <div className="container">
         <div className="row align-center justify-between">
           <div id="logo" className="col-25">
-            <img src="../public/img/dc-logo.png" alt="Logo" />
+            <img src="/img/dc-logo.png" alt="Logo" />
           </div>
           <div className="list col-75">
             <ul className="d-flex">
